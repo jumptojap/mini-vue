cd7a4cb6e25de9735626fb770159112cdad3ec0a
eastmoon <zhuz3856@gmail.com>
2025-06-09 22:37:43 +0800

    初始提交

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',           // 入口文件
+    output: {
+        filename: 'mini-vue.js',           // 输出文件名
+        path: path.resolve(__dirname, 'dist'), // 输出路径必须是绝对路径
+        publicPath: '/',
+        clean: true                      // 每次构建清除 dist 目录
+    },
+    mode: 'development',               // 开发模式（也可改成 production）
+    devtool: 'source-map',              // 方便调试源码
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        open: true, // 自动打开浏览器
+    }
+};
